e7ab61ae139a90a1cc71b361d0477548f01cf91f
VS_KUZNEC <sevactet.games@gmail.com>
2024-07-04 17:20:42 +0300

    news

--- Project_25/BookRepository.cs
+++ Project_25/BookRepository.cs
@@ -11,6 +11,8 @@ namespace Project_25
     {
         private readonly AppContext _context;
 
+        private readonly List<Book> _books = new List<Book>();
+
         public BookRepository(AppContext context)
         {
             _context = context;
@@ -18,7 +20,7 @@ namespace Project_25
 
         public Book GetById(int id)
         {
-            return _context.Books.FirstOrDefault(b => b.Id == id);
+            return _context.Books.FirstOrDefault(u => u.Id == id);
         }
 
         public IEnumerable<Book> GetAll()
@@ -52,6 +54,59 @@ namespace Project_25
                 _context.SaveChanges();
             }
         }
-        
+
+        public List<Book> BooksByGenreAndYear(string genre, int startYear, int endYear)
+        {
+            var query = _books.Where(u => u.Genre == genre && u.Year >= startYear && u.Year <= endYear); 
+            return query.ToList();
+        }
+
+        public int BooksCountByAuthor(string author)
+        {
+            var query = _books.Where(u => u.Author == author);
+            return query.Count();
+        }
+
+        public int BooksCountByGenre(string genre)
+        {
+            var query = _books.Where(u => u.Genre == genre);
+            return query.Count();
+        }
+
+        public bool BooksExists(string author, string title)
+        {
+            var query = _books.Where(u => u.Author == author && u.Title == title);
+            return query.Any();
+        }
+
+        public bool BooksCheckedOut(int bookId, int userId)
+        {
+            var query = _books.Where(u => u.Id == bookId && u.Id == userId);
+            return query.Any();
+        }
+
+        public int BooksCountByUser(int userId)
+        {
+            var query = _books.Where(u => u.Id == userId);
+            return query.Count();
+        }
+
+        public Book BooksLast()
+        {
+            var query = _books.OrderByDescending(u => u.Year);
+            return query.FirstOrDefault();
+        }
+
+        public List<Book> BooksSortedByName()
+        {
+            var query = _books.OrderBy(u => u.Title);
+            return query.ToList();
+        }
+
+        public List<Book> BooksSortedByYearDescending()
+        {
+            var query = _books.OrderByDescending(u => u.Year);
+            return query.ToList();
+        }
     }
 }
--- Project_25/Program.cs
+++ Project_25/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Identity.Client;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace Project_25
 {
@@ -8,13 +9,29 @@ namespace Project_25
         {
             using (var db = new AppContext())
             {
+                var user = db.Users.Include(u => u.Book).FirstOrDefault();
+
+                Console.WriteLine($"У пользователя: {user.Name} есть книга: {user.Book.Title} выпуска {user.Book.Year}г");
+            }
+        }
+
+        public static void FillData()
+        {
+            using (var db = new AppContext())
+            {
                 var user1 = new User { Name = "Arthur", Email = "seva@gmail.com" };
                 var book1 = new Book { Title = "Think and get rich", Year = "1937" };
                 var author1 = new Author { FirstName = "УГУ", LastName = "ГУГУ" };
                 var genre1 = new Genre { Name = "Экшен" };
 
+                book.Author = author;
+                book.Genre = genre;
+
+                user.Book = book;
+
                 db.Users.Add(user1);
                 db.Books.Add(book1);
+
                 db.SaveChanges();
             }
         }
